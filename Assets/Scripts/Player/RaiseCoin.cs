3fe105d8800ed1304774f617e8477777ca494974
Dirofu <if-sd@mail.ru>
2021-04-24 04:48:03 +0700

    init

---
+++ Assets/Scripts/Player/RaiseCoin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class RaiseCoin : MonoBehaviour
+{
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            _audio.Play();  
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSettings/GameFinish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFinish : MonoBehaviour
+{
+    [SerializeField] private Coin[] _coins;
+
+    private void Update()
+    {
+        _coins = gameObject.GetComponentsInChildren<Coin>();
+
+        if (_coins.Length == 0)
+        {
+            Debug.Log("Game Over.");
+        }
+    }
+}
